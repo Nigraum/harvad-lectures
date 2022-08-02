bdf6a538ba0322ae72586dc681c2102fc956dff8
Nigraum <yan.corintima@hotmail.com>
2022-08-02 16:15:57 -0300

    Finishing meow.c

--- C/meow.c
+++ C/meow.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-void meow(void)
-{
-    printf("meow\n");
-}
+void meow(int n);
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
+    meow(3);
+}
+
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
     {
-        meow();
+        printf("meow\n");
     }
 }
